814a477ded567b785b27de023ed7dac3bf7c0515
serkanh <shaytac@gmail.com>
2025-05-07 17:28:34 -0400

    updated

--- agent_root/agent.py
+++ agent_root/agent.py
@@ -1,6 +1,9 @@
 from google.adk.agents import Agent, LlmAgent
 from .tools.kube_tools import *
 from google.adk.models.lite_llm import LiteLlm
+from contextlib import AsyncExitStack
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters, SseServerParams
+import os
 
 kubernetes_agent = Agent(
     name="kubernetes_agent",
@@ -31,14 +34,38 @@ kubernetes_agent = Agent(
 
 
 
-sre_agent = LlmAgent(
+
+async def create_cost_analysis_agent():
+  """Gets tools from MCP Server."""
+  common_exit_stack = AsyncExitStack()
+
+  local_tools, _ = await MCPToolset.from_server(
+      connection_params=StdioServerParameters(
+          # Use this for AWS Cost Analysis
+          command='uvx',
+          args= ["awslabs.cost-analysis-mcp-server@latest"],
+          env={
+            "FASTMCP_LOG_LEVEL": "ERROR",
+            "AWS_PROFILE": os.environ.get("AWS_PROFILE", "default")
+            },
+            disabled = False,
+            autoApprove = []
+      ),
+      async_exit_stack=common_exit_stack
+  )
+
+  agent = LlmAgent(
     name="sre_agent",
     model=LiteLlm(model="bedrock/arn:aws:bedrock:us-east-1:827541288795:inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0"),
-    description="An assistant that can help you with your Kubernetes cluster",
-    instruction="You are experienced SRE/Devops expert specialized in AWS, Kubernetes and various tools that are relevant to the cloud native ecosystem.",
-    sub_agents=[
-        kubernetes_agent,
+    description="An assistant to help you with your AWS Cost Analysis",
+    instruction="You are an expert in AWS Cost Analysis and you can help me with my AWS Cost Analysis",
+    tools=[
+        *local_tools,
     ],
 )
+  return agent, common_exit_stack
 
-root_agent = sre_agent
+cost_analysis_agent = create_cost_analysis_agent()
+
+
+root_agent = create_agent()
