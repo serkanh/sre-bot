052cb5bd2f6a83c8ace3268a0d7a650e02e89589
serkanh <shaytac@gmail.com>
2025-05-07 20:51:33 -0400

    aws_core mcp added

--- agent_root/agent.py
+++ agent_root/agent.py
@@ -37,10 +37,35 @@ async def kubernetes_agent():
     return agent, exit_stack
 
 
+async def get_aws_core_mcp():
+  """Gets tools from MCP Server."""
+  tools, exit_stack = await MCPToolset.from_server(
+      connection_params=StdioServerParameters(
+          command='uvx',
+          args=[ 
+            "awslabs.core-mcp-server@latest"
+        ],
+        env={
+            "AWS_PROFILE": os.getenv("AWS_PROFILE"),
+        },
+        autoApprove=[],
+        disabled= False
+      )
+  )
+
+  agent = LlmAgent(
+      model=LiteLlm(model="bedrock/arn:aws:bedrock:us-east-1:827541288795:inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0"),
+      name='aws_core_mcp_agent',
+      instruction='AWS Related queries.',
+      tools=tools,
+  )
+  return agent, exit_stack
+    
+
 
 async def create_root_agent():
-    k8s_agent, k8s_exit_stack = await kubernetes_agent()
-
+    k8s_agent, exit_stack = await kubernetes_agent()
+    aws_core_mcp_agent, exit_stack = await get_aws_core_mcp()
     agent = Agent(
         name="root_agent",
         model=LiteLlm(model="bedrock/arn:aws:bedrock:us-east-1:827541288795:inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0"),
@@ -49,13 +74,15 @@ async def create_root_agent():
         You are an helpful assistant who tries to understand the user's request and delegates it to the most appropriate sub agent.
         Here are the sub agents available:
         - kubernetes_agent: for Kubernetes related queries
+        - aws_core_mcp_agent: for AWS Cost Analysis
         """,
         sub_agents=[
             k8s_agent,
+            aws_core_mcp_agent
         ]
     )
         
-    return agent, k8s_exit_stack
+    return agent, exit_stack
 
 
 async def get_root_agent():
